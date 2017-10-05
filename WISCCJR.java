3509800ee78a405ebdee2cb81b086822993d3250
Luckystratjacket <luckystratjacket1@Gmail.com>
2017-10-05 14:09:53 -0400

    First Save
    Introduces the WISCCJR File

---
+++ WISCCJR.java
@@ -0,0 +1,20 @@
+public class WISCCJR {
+    public static String compress(String s) {
+        /* Sam, Winnie, Claire, Jackie
+
+		Specifications
+		Must be best compression, including nested blocks
+		If best compression has same length as original, return original
+		*/
+    }
+    public static String decompress(String s) {
+		/* Richard, Ian, Caleb
+
+		Specifications
+		Must be best compression, including nested blocks
+		If best compression has same length as original, return original
+		*/ 
+	}
+
+
+}
